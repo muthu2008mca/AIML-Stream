20f74f17df57963b622c081b95249617dd09d4f7
muthuganesh <muthu2008mca@gmail.com>
2025-07-23 23:36:41 +0530

    updated changed

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-import pickle
-from flask import Flask, request, render_template
+import streamlit as st
 import numpy as np
-import math
+import pickle
 
-app = Flask(__name__)
-model2 = pickle.load(open('model.pkl','rb'))
+# Load the model
+model = pickle.load(open("model.pkl", "rb"))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+st.title("🎬 Movie Interest Predictor")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    int_feature = [int(i) for i in request.form.values()] #[1,2,3,5]
+# User 1
+st.subheader("User 1")
+age1 = st.number_input("Age (User 1)", min_value=1, max_value=100, value=25)
+gender1 = st.radio("Gender (User 1)", ["Male", "Female"])
+gender1_val = 1 if gender1 == "Male" else 0
 
-    final_feature = np.array(int_feature).reshape(1, -1)
+# User 2
+st.subheader("User 2")
+age2 = st.number_input("Age (User 2)", min_value=1, max_value=100, value=30)
+gender2 = st.radio("Gender (User 2)", ["Male", "Female"])
+gender2_val = 1 if gender2 == "Male" else 0
 
-    prediction = model2.predict(final_feature) #[[1,2,3,4]]
-    output = round(prediction[0],2)
-
-    return render_template('index.html', predict_text = 'Number of weekly rides {}'.format(math.floor(output)))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if st.button("Predict"):
+    input_data = np.array([[age1, gender1_val], [age2, gender2_val]])
+    prediction = model.predict(input_data)
+    st.success(f"Prediction for User 1: {prediction[0]}")
+    st.success(f"Prediction for User 2: {prediction[1]}")
